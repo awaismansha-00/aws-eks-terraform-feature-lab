ab980206db3744eb2f5964ab446014b9bb130678
awais <awaismansha97@gmail.com>
2026-07-09 23:25:46 +0100

    local updated

--- environments/dev/locals.tf
+++ environments/dev/locals.tf
@@ -1,3 +1,4 @@
+# This file contains local values for the development environment configuration.
 locals {
   name_prefix  = "${var.environment}-${var.project_name}"
   cluster_name = local.name_prefix
